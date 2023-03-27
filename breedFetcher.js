468e290e4f8a2d7f65a512c27fbfd2224e4550d2
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-03-26 20:34:25 -0700

    Refactor and add fetchBreedDescription

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,24 @@
 const request = require('request');
-// retrieve a cat breed description by entering a breed name as a command line argument
-const breedFetcher = function() {
-  const url = "https://api.thecatapi.com/v1/breeds/search";
-  const args = process.argv.slice(2);
-  const searchParam = url + "?q=" + args[0];
-  // make an HTTP request
-  request(searchParam, (error, response, body) => {
+
+const fetchBreedDescription = function(breedName, callback) {
+  request("https://api.thecatapi.com/v1/breeds/search?q=" + breedName, (error, response, body) => {
     if (error) {
       console.log("error:", error);
       console.log("statusCode:", response && response.statusCode);
     }
     const data = JSON.parse(body);
-    if (!data.length) {
-      // if data is an empty array
-      console.log("Sorry, breed not found!");
-    } else {
-      // log the breed description to the console
-      console.log(data[0].description);
+    
+    if (error) {
+      callback(error, null);
+      return;
     }
+  
+    if (!error) {
+      callback(null, data[0].description);
+      return;
+    }
+
   });
 };
 
-breedFetcher();
+module.exports = { fetchBreedDescription };
